0412209cd0ec9ce9c851e98346449b5e20dbeba6
Say Pulung <saypulung@gmail.com>
2026-05-05 15:44:12 +0700

    chore(frotn): Fix blank page

--- frontend/image-browser-app/src/main.ts
+++ frontend/image-browser-app/src/main.ts
@@ -1,11 +1,5 @@
-import { LOCALE_ID } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-import { registerLocaleData } from '@angular/common';
-import localeId from '@angular/common/locales/id';
-
-registerLocaleData(localeId);
-
-bootstrapApplication(AppComponent, { ...appConfig, providers: [{ provide: LOCALE_ID, useValue: 'id-ID' }] }).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
